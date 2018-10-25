0ec5a828e26a1989133779ba33979be4867e7f34
HannaZahorodnia <zahorodnia.h@gmail.com>
2018-10-25 22:18:06 +0200

    Оптимизирует шрифты, минимизирует скрипты  и ровняет верстку по pixelperfect

--- source/js/script.js
+++ source/js/script.js
@@ -5,7 +5,7 @@ var navToggle = document.querySelector('.main-navigation__toggle');
 
 navMain.classList.remove('main-navigation--nojs');
 
-navToggle.addEventListener('click', function() {
+navToggle.addEventListener('click', function () {
   if (navMain.classList.contains('main-navigation--closed')) {
     navMain.classList.remove('main-navigation--closed');
     navMain.classList.add('main-navigation--opened');
@@ -18,25 +18,59 @@ navToggle.addEventListener('click', function() {
 //Открытие/закрытие модального окна
 
 var orderLink = document.querySelector(".weekly-hit__order");
+var cartIcons = document.querySelectorAll(".catalog__add-to-cart");
 var cartPopup = document.querySelector(".modal");
 var cartForm = cartPopup.querySelector("form");
-var body = document.querySelector('body');
+var sizes = cartForm.querySelectorAll('input');
+var overlay = document.querySelector(".overlay");
+var submitButton = cartForm.querySelector('button');
+var ESC = 27;
 
-orderLink.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  cartPopup.classList.add("modal--show");
-  evt.stopPropagation();
-});
+var onEscPress = function (evt) {
+  if (evt.keyCode === ESC) {
+    closePopup();
+  }
+};
+
+if (orderLink) {
+  orderLink.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    cartPopup.classList.add("modal--show");
+    evt.stopPropagation();
+    overlay.classList.add("overlay--show");
+    document.addEventListener('keydown', onEscPress);
+  });
+} else if (cartIcons) {
+  for (var i = 0; i < cartIcons.length; i++) {
+    var element = cartIcons[i];
+    element.addEventListener('click', function (evt) {
+      evt.stopPropagation();
+      showPopup();
+      document.addEventListener('keydown', onEscPress);
+    });
+  }
+}
 
 cartForm.addEventListener("submit", function (evt) {
-  evt.preventDefault();
+  if (!sizes[0].checked && !sizes[1].checked && !sizes[2].checked) {
+    evt.preventDefault();
+    alert("Пожалуйста, выберите нужный размер");
+  }
 });
+
 
 var closePopup = function () {
   cartPopup.classList.remove('modal--show');
+  overlay.classList.remove("overlay--show");
+  document.removeEventListener('keydown', onEscPress);
 };
 
-document.addEventListener('click', function (evt) {
+var showPopup = function () {
+  cartPopup.classList.add("modal--show");
+  overlay.classList.add("overlay--show");
+}
+
+overlay.addEventListener('click', function (evt) {
   var target = evt.target;
   if (!cartPopup.contains(target)) {
     closePopup();
